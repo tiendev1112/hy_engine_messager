c768f620cf5f45c57a7df77c8b4704a9db2f83ed
maguon <info@myxxjs.com>
2021-01-21 09:41:06 +0800

    add persist redux and set current user

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import LoginScreen from './login/LoginScreen';
 import ChatItemScreen from './main/ChatItemScreen';
+import ChatMediaModal from './main/ChatMediaModal';
 import ChatPanel from './main/ChatPanel';
 import ContactPanel from './main/ContactPanel';
 import SettingPanel from './main/SettingPanel';
+import WelcomeScreen from './main/WelcomeScreen';
 
 
 export {
-    LoginScreen,ChatItemScreen,ChatPanel,ContactPanel,SettingPanel
+    LoginScreen,ChatItemScreen,ChatMediaModal,ChatPanel,ContactPanel,SettingPanel,WelcomeScreen
 }
